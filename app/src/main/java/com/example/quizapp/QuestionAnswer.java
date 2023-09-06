af1707703eb8946d5b424c4d387b04dde3488993
Ankit <ankitkumar1990asap@gmail.com>
2023-09-06 20:36:26 +0530

    files

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "Who is the father of Computers?",
+            "Which one the first web browser invented in 1990?",
+            "Which programming language used to create program like applets?",
+            "Which of the following computer language is written in binary codes only?",
+            "Which of the following is the smallest unit of data in a computer?"
+    };
+    public static String choices[][] = {
+            {"James Gosling","Charles Babbage","Dennis Ritchie","Bjarne Stroustrup"},
+            {"Internet Explorer","Mosaic","Mozilla","Nexus"},
+            {"COBOL","C language","Java","BASIC"},
+            {"Pascal","Machine Language","C","C#"},
+            {"Bit","KB","Nibble","Byte"}
+    };
+    public static String correctAnswer[] = {
+            "Charles Babbage",
+            "Nexus",
+            "Java",
+            "Machine Language",
+            "Bit"
+    };
+}
